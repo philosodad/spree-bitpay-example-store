7eee283d40c871fb43e340cf13d214da32893db8
Carrot Ironfounderson <carrot@gavin.example.com>
2014-09-12 14:48:24 +0000

    modified cap file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,58 +1,35 @@
-# config valid only for Capistrano 3.1
-lock '3.2.1'
+require "bundler/capistrano"
+load "deploy/assets"
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, "BitpayExampleStore"
+set :repository,  "git@github.com:philosodad/spree-bitpay-example-store.git"
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+server = "50.116.35.154"
+role :web, "50.116.35.154"                         # Your HTTP server, Apache/etc
+role :app, "50.116.35.154"                         # This may be the same as your `Web` server
+role :db,  "50.116.35.154", :primary => true # This is where Rails migrations will run
 
-# Default value for :scm is :git
-# set :scm, :git
+set :user, "carrot"
+set :deploy_to, "/srv/www/#{application}"
+set :user_sudo, false
 
-# Default value for :format is :pretty
-# set :format, :pretty
+default_run_options[:shell] = '/bin/bash --login'
+default_environment["RAILS_ENV"] = 'production'
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
 
-# Default value for :pty is false
-# set :pty, true
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
 
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-end
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
